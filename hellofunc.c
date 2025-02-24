224c62a1c969e5504e01b53b8afa2c4dcd2797a6
TerenceKutzner <terence.kutzner@hotmail.com>
2025-02-24 11:51:50 -0300

    added line for Gerencia de Configuracao

--- hellofunc.c
+++ hellofunc.c
@@ -2,6 +2,8 @@
 #include <hellomake.h>
 
 void myPrintHelloMake(void) { 
+    printf("Terence was Here\n");
+    printf("Gerencia de Configuracao");
     printf("Hello my friend Makefile!\n");
     return; 
 }
